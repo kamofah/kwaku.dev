f6c84c99d31d87b0db51e00b47418d3d7a8515d4
Kwaku Amofah-Boafo <boafo819@gmail.com>
2026-03-24 05:02:02 -0400

    Refactor index.html and style.css for improved structure and styling. Updated header and navigation classes for better semantics, enhanced hero section with new content and layout, and refined CSS variables and styles for consistency. Removed unused node logo file.

---
+++ data/projects.ts
@@ -0,0 +1,31 @@
+export const PROJECTS = [
+    {
+        id: 1,
+        name: "Queued Up",
+        techStack: ["React", "TypeScript", "Supabase"],
+        image: "TODO: ADD LINK TO IMAGE",
+        link: "https://queuedup.com"
+    },
+    {
+        id: 2,
+        name: "BeHold",
+        techStack: ["React Native", "TypeScript", "Supabase"],
+        image: "TODO: ADD LINK TO IMAGE",
+        link: "https://behold.com"
+    },
+    {
+        id: 3,
+        name: "Prelude",
+        techStack: ["React", "TypeScript", "Supabase"],
+        image: "TODO: ADD LINK TO IMAGE",
+        link: "https://prelude.com"
+    },
+    {
+        id: 4,
+        name: "MAWB",
+        techStack: ["React Native", "TypeScript", "Supabase"],
+        image: "TODO: ADD LINK TO IMAGE",
+        link: "https://mawb.com"
+    }
+    
+]
